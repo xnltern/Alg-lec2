f0002393ed2e2f7d0a060575451f43c33476a2e0
xnltern <158267235+xnltern@users.noreply.github.com>
2024-02-14 13:50:07 +0300

    Create 2.2.py

---
+++ 2.2.py
@@ -0,0 +1,13 @@
+x = int(input("Введите число: "))
+y = int(input("Введите число: "))
+if x<0:
+    z = max(x,y)
+if x>=0:
+    z = min(x,y)
+if y >=0:
+    z = min(x,y)
+if y<0:
+    z = max(x**2,y**2)
+else:
+    z = min(x,y)+0,5/1+max**2 *(x,y)
+print(z)   
