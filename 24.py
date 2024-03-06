3b44a6203dc07abbda2fa7d91f8830a1f081b29b
xnltern <158267235+xnltern@users.noreply.github.com>
2024-03-06 11:14:56 +0300

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,10 @@
+x = int(input("Введите число: "))
+y = int(input("Введите число: "))
+if x > 0 and y > 0:
+    print("1")
+if x < 0 and y > 0:
+    print("2 ")
+if x < 0 and y < 0:
+    print("3")
+if x > 0 and y < 0:
+    print("4 ")
